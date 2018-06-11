1b9098097762e291469224e8ac5fcef4eab75d76
Anant Kumar <278anant@gmail.com>
2018-06-11 17:15:13 +0530

    1. Added test code for pyserial 2. Added code to input command from user

--- test1.py
+++ test1.py
@@ -1,4 +1,6 @@
 import sys
+import serial
+from time import sleep
 from PyQt4 import QtGui, QtCore, uic
 
 qtCreatorFile = "groundstationapp.ui"  # Enter file here.
@@ -38,7 +40,11 @@ class Window(QtGui.QMainWindow, Ui_MainWindow):
         fileMenu = mainMenu.addMenu('&Command List')
         fileMenu.addAction(extractAction2)
 
+        #self.text_command = QtGui.QLineEdit(self)
+        #self.text_command.move(100,22)
+        #self.text_command.setPlaceholderText("Enter Command to Transmit")
 
+        self.command_str = ''
         self.show()
 
     def close_application(self):
@@ -46,11 +52,25 @@ class Window(QtGui.QMainWindow, Ui_MainWindow):
         sys.exit()
 
     def transmit(self):
-        print("Transmitting")
+        text, result = QtGui.QInputDialog.getText(self, "Input Command", "Please Enter Command")
+        if result == True:
+            self.command_str = str(text)
+        print("Transmitting This: ", self.command_str)
         pass
 
     def recieve(self):
+        #Test code for checking working of pyserial with arduino
         print("Recieving")
+        ser = serial.Serial('/dev/tty.COM1', 9600)  # Establish the connection on a specific port
+        counter = 32  # Below 32 everything in ASCII is gibberish
+
+        while True:
+            counter += 1
+            ser.write(str(chr(counter)))  # Convert the decimal number to ASCII then send it to the Arduino
+            print(ser.readline())  # Read the newest output from the Arduino
+            sleep(.1)  # Delay for one tenth of a second
+            if counter == 255:
+                counter = 32
         pass
 
     def getCommandList(self):
